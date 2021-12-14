9ff68aabcff94f6fc269cc1aad3030b7bfbb98a2
mo4718al-s <mo4718al-s@student.lu.se>
2021-12-14 21:22:51 +0100

    ready to deploy

--- main.py
+++ main.py
@@ -8,14 +8,41 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.clock import Clock
+from kivy.core.window import Window
+from kivy.uix.scrollview import ScrollView
 import socket_client
 import sys
-
 import os
 
 # print(kivy.__version__)
 kivy.require("2.0.0")
 
+
+class ScrollableLabel(ScrollView):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.layout = GridLayout(cols = 1, size_hint_y = None)
+        self.add_widget(self.layout)
+
+        self.chat_history  = Label(size_hint_y = None, markup = True) # markup to give different colors to different users
+        self.scroll_point = Label() # just to stop scrolling to this point
+
+        self.layout.add_widget(self.chat_history)
+        self.layout.add_widget(self.scroll_point)
+
+    def update_chat_history(self, message):
+        self.chat_history.text += '\n' + message
+        self.layout.height = self.chat_history.texture_size[1] + 15
+        self.chat_history.height = self.chat_history.texture_size[1]
+        self.chat_history.text_size = (self.chat_history.width*0.98, None)
+
+        self.scroll_to(self.scroll_point)
+        #self.scroll_y = 0
+
+    def update_chat_history_layout(self, _ = None):
+        self.layout.height = self.chat_history.texture_size[1] + 15
+        self.chat_history.height = self.chat_history.texture_size[1]
+        self.chat_history.text_size = (self.chat_history.width * 0.98, None)
 
 class LoginPage(GridLayout):
     def __init__(self, **kwargs):
@@ -98,7 +125,69 @@ class ChatPage(GridLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.cols = 1
-        self.add_widget(Label(text = "Hey it works"))
+        self.rows = 2
+
+        self.history = ScrollableLabel(height = Window.size[1]*0.9, size_hint_y = None)
+        self.add_widget(self.history)
+
+        self.new_message = TextInput(width= Window.size[0]*0.8, size_hint_x = None, multiline = False)
+        self.send = Button(text = "Send")
+        self.send.bind(on_press= self.send_message)
+
+        bottom_line = GridLayout(cols=2)
+        bottom_line.add_widget(self.new_message)
+        bottom_line.add_widget(self.send)
+        self.add_widget(bottom_line)
+
+        Window.bind(on_key_down = self.on_key_down)
+
+        Clock.schedule_once(self.focus_text_input, 1)
+        socket_client.start_listening(self.incoming_message, show_error)
+        self.bind(size = self.adjust_fields)
+
+    def adjust_fields(self, *_):
+
+        # Chat history height - 90%, but at least 50px for bottom new message/send button part
+        if Window.size[1] * 0.1 < 50:
+            new_height = Window.size[1] - 50
+        else:
+            new_height = Window.size[1] * 0.9
+        self.history.height = new_height
+
+        # New message input width - 80%, but at least 160px for send button
+        if Window.size[0] * 0.2 < 160:
+            new_width = Window.size[0] - 160
+        else:
+            new_width = Window.size[0] * 0.8
+        self.new_message.width = new_width
+
+        # Update chat history layout
+        #self.history.update_chat_history_layout()
+        Clock.schedule_once(self.history.update_chat_history_layout, 0.01)
+
+    def on_key_down(self, instance, keyboard, keycode, text, modifiers):
+        if keycode == 40:
+            self.send_message(None)
+
+
+
+
+    def send_message(self, _):
+        message = self.new_message.text
+        self.new_message.text = ""
+        if message:
+            self.history.update_chat_history(f'[color=dd2020] {chat_bot.connect_page.username.text}[/color] > {message}')
+            socket_client.send(message)
+
+        Clock.schedule_once(self.focus_text_input, 0.1)
+
+    def focus_text_input(self, _):
+        self.new_message.focus = True
+
+    def incoming_message(self, username, message):
+        self.history.update_chat_history(f'[color=20dd20]{username}[/color] > {message}')
+
+
 
 
 class MainScreen(App):
